15de9ec2eb70027f1c3361c0010f28c19a301f10
Rex Hygate <31773824+RexShinka@users.noreply.github.com>
2018-09-08 15:42:36 -0500

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-var Migrations = artifacts.require("Migrations");
+var Migrations = artifacts.require("./Migrations.sol");
+var Wayback = artifacts.require("./Wayback.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Wayback);
 };
