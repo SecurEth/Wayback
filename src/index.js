7ac3f2ec4b76714ab2b9d80f58f8625f8728c832
Roman Pavlovskyi <roman@pumpup.co>
2018-09-08 19:09:26 +0200

    :star: Use React Context API with drizzle

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router'
-import { DrizzleProvider } from 'drizzle-react'
+import { Drizzle } from "drizzle";
+import { DrizzleContext } from "drizzle-react";
 
 // Layouts
 import App from './App'
-import { LoadingContainer } from 'drizzle-react-components'
+// import { LoadingContainer } from 'drizzle-react-components'
 
 import { history, store } from './store'
 import drizzleOptions from './drizzleOptions'
 
+// Setup the drizzle instance.
+const drizzle = new Drizzle(drizzleOptions, store);
+
 ReactDOM.render((
-    <DrizzleProvider options={drizzleOptions} store={store}>
-      <LoadingContainer>
+    <DrizzleContext.Provider drizzle={drizzle}>
+      {/* <LoadingContainer> */}
         <Router history={history} store={store}>
           <Route exact path="/" component={App} />
         </Router>
-      </LoadingContainer>
-    </DrizzleProvider>
+      {/* </LoadingContainer> */}
+    </DrizzleContext.Provider>
   ),
   document.getElementById('root')
 );
